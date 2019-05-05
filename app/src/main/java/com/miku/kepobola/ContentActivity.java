c5900806944022c7d49723f7a7846302dab58caa
mwiguna <mwiguna33@gmail.com>
2019-05-05 17:56:50 +0700

    Enable Video Fullscreen on Webview Content

--- app/src/main/java/com/miku/kepobola/ContentActivity.java
+++ app/src/main/java/com/miku/kepobola/ContentActivity.java
@@ -1,10 +1,16 @@
 package com.miku.kepobola;
 
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.FrameLayout;
 
 public class ContentActivity extends AppCompatActivity {
 
@@ -21,15 +27,91 @@ public class ContentActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        initWebview();
+        initWebview(savedInstanceState);
     }
 
-    private void initWebview() {
+    private void initWebview(Bundle savedInstanceState) {
         myBrowser = findViewById(R.id.mybrowser);
+        myBrowser.setWebChromeClient(new FullscreenVideoEnable());
         myBrowser.getSettings().setJavaScriptEnabled(true);
         myBrowser.getSettings().setBuiltInZoomControls(true);
         myBrowser.getSettings().setDisplayZoomControls(false);
-        myBrowser.loadUrl("file:///android_asset/" + getIntent().getStringExtra("fileContent"));
+
+        // Run Thread for Video Fullscreen
+
+        if(savedInstanceState==null){
+            myBrowser.post(new Runnable() {
+                @Override
+                public void run() {
+                    myBrowser.loadUrl("file:///android_asset/" + getIntent().getStringExtra("fileContent"));
+                }
+            });
+        }
+    }
+
+    // Fullscreen Video Setting
+    // Activity Tag Manifest
+
+    private class FullscreenVideoEnable extends WebChromeClient {
+
+        private View mCustomView;
+        private WebChromeClient.CustomViewCallback mCustomViewCallback;
+        private int mOriginalOrientation;
+        private int mOriginalSystemUiVisibility;
+
+        FullscreenVideoEnable() {}
+
+        public Bitmap getDefaultVideoPoster()
+        {
+            if (mCustomView == null) {
+                return null;
+            }
+            return BitmapFactory.decodeResource(getApplicationContext().getResources(), 2130837573);
+        }
+
+        public void onHideCustomView()
+        {
+            ((FrameLayout)getWindow().getDecorView()).removeView(this.mCustomView);
+            this.mCustomView = null;
+            getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
+            setRequestedOrientation(this.mOriginalOrientation);
+            this.mCustomViewCallback.onCustomViewHidden();
+            this.mCustomViewCallback = null;
+        }
+
+        public void onShowCustomView(View paramView, WebChromeClient.CustomViewCallback paramCustomViewCallback)
+        {
+            if (this.mCustomView != null)
+            {
+                onHideCustomView();
+                return;
+            }
+            this.mCustomView = paramView;
+            this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
+            this.mOriginalOrientation = getRequestedOrientation();
+            this.mCustomViewCallback = paramCustomViewCallback;
+            ((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
+            getWindow().getDecorView().setSystemUiVisibility(3846);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState )
+    {
+        super.onSaveInstanceState(outState);
+        myBrowser.saveState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        myBrowser.restoreState(savedInstanceState);
     }
 
     @Override
